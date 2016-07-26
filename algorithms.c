b24187917a0fd2fff1d12a2fb141036eadbfba87
mattdm94 <mattdm@iastate.edu>
2016-07-25 20:54:52 -0500

    Update algorithms.c

--- algorithms.c
+++ algorithms.c
@@ -1,5 +1,6 @@
 #include "algorithms.h"
-
+#include "math.h"
+#define INT_MAX 32767(*Math.pow(2,15)-1)
 // ============================================================================
 // Recursive functions
 // ----
@@ -10,20 +11,77 @@
 
 struct Link * get_minimum_r(struct Link * link, size_t order_by, struct Link * current_min){
 
-    // TODO: Fill in the code
+    if(order_by == 0){
+        link = cpm_by_a();
+    }
+    else if(order_by == 1){
+        link = cmp_by_b();
+        }
+        else{
+            printf("Please enter a 0 or 1");
+        }
 
     return link;
 }
 
 void recursive_print_r(struct Link * link, size_t order_by, struct Link * stop_link){
-
-    // TODO: Fill in the code
+  int smallest = INT_MAX;
+  struct Link * current = link;
+  int i = 0;
+  struct Link * final = link;
+  
+    if(order_by == 0){
+         while(link->next != final){
+            struct Link * current = link;
+            if(link->p[0] < link->next->p[0]){
+                link = link->next;
+            }
+            while(link->next.p[0] < link->p[0]){
+                if(link->next->p[0] < link->p[0]){
+                    
+                }
+            }
+        }
+        while(link->next != final){
+            print_link(link);
+            link = link->next;
+        }
+    }
+    
+    
+    
+    else if(order_by == 1){
+       
+        while(link->next != final){
+            print_link(link);
+            link = link ->next;
+        }
+    }
+    
+    
+    
+    
+    
+    else{
+        printf("Please enter a valid value");
+    }
+    
+    
 
 }
 
 struct Link * winnow_r(struct Link * link, float threshold, struct Link * stop_link){
-
-    // TODO: Fill in the code
+    // insert code for stop link
+    while(link != stop_link){
+        if(link.score < threshold){
+            delete_link(link);
+            link = link->next;
+        }
+        else {
+            link = link->next;
+        }
+        
+    }
 
     return link;
 }
